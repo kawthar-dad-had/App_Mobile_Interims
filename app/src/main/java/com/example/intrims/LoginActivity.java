be737f2b332945333990b0b7c36ba9f065103460
kawthar-dad-had <kawdad24@gmail.com>
2024-03-27 15:26:51 +0100

    Add AppBar

--- app/src/main/java/com/example/intrims/LoginActivity.java
+++ app/src/main/java/com/example/intrims/LoginActivity.java
@@ -66,8 +66,12 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (username.isEnabled() && password.isEnabled() && username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                     Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
                 } else {
                     Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
                 }
             }
         });
